ee4331585098afd4a9726a5b941a86833b9d3eb2
Rene Rojas <re.renerojas@gmail.com>
2026-02-03 12:42:05 -0300

    Integrações com o docker-compose e banco de dados

--- MinhaApi/obj/Debug/net10.0/MinhaApi.AssemblyInfo.cs
+++ MinhaApi/obj/Debug/net10.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c7a2b6bdcfbec88fbb6929f29cc1b37c72e778")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
