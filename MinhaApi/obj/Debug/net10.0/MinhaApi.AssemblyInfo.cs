c48669782edbba1d8761ce431eb3cbb394554841
Rene Rojas <re.renerojas@gmail.com>
2026-02-11 14:29:27 -0300

    Criação do coverage

--- MinhaApi/obj/Debug/net10.0/MinhaApi.AssemblyInfo.cs
+++ MinhaApi/obj/Debug/net10.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adaaab9ee3b0036e3e784983e994cdbe08f09e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52bb4a20575b7ef48c458012282112aef3ad588b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
